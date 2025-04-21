1101a0438759d9fc510390e7d507fbad85d10685
Эду Эду1 <0563166@gmail.com>
2025-04-21 22:10:11 +0300

    Methods have been implemented

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.UUID;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,33 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        storage = new Resume[10000];
     }
 
     void save(Resume r) {
+        r.uuid = UUID.randomUUID().toString();
     }
 
-    Resume get(String uuid) {
-        return null;
+    String get(String uuid) {
+
+        return uuid;
     }
 
     void delete(String uuid) {
+
+        uuid = uuid == null ? "" : uuid;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
+
         return new Resume[0];
     }
 
     int size() {
-        return 0;
+
+        return storage.length;
     }
 }
