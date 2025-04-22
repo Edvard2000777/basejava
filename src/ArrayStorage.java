aa64fec8b0ab2f78b270cff4ad55c3ad342199e5
Эду Эду1 <0563166@gmail.com>
2025-04-22 12:39:36 +0300

    Methods have been implemented

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.UUID;
 
 /**
@@ -5,35 +6,62 @@ import java.util.UUID;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private static int counter = 0;
 
     void clear() {
-        storage = new Resume[10000];
+        for (int i = 0; i < counter; i++) {
+            storage = new Resume[0];
+        }
+        counter = 0;
     }
 
     void save(Resume r) {
-        r.uuid = UUID.randomUUID().toString();
+        if (r == null) throw new
+                IllegalArgumentException("resume is null");
+        if (counter == 0) storage[0] = r;
+        else storage[counter] = r;
+        counter++;
+
     }
 
     String get(String uuid) {
-
-        return uuid;
+        for (int i = 0; i < counter; i++) {
+            if (this.storage[i].uuid.equals(uuid)) {
+                return this.storage[i].toString();
+            }
+        }
+        return null;
     }
 
     void delete(String uuid) {
 
-        uuid = uuid == null ? "" : uuid;
+        for (int i = 0; i < counter; i++) {
+            if (this.storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[i - 1];
+                storage[i - 1] = null;
+                counter--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-
-        return new Resume[0];
+        Resume[] resumes = new Resume[counter];
+        for (int i = 0; i < counter; i++) {
+            resumes[i] = storage[i];
+        }
+        return resumes;
     }
 
     int size() {
 
-        return storage.length;
+        return counter;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(storage);
     }
 }
